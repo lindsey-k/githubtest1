dcdb93f264b3f6dec2c73eb39e94862b13251a10
lindsey-k <doyoung.kwon7251@gmail.com>
2025-09-02 18:19:57 -0600

    Update bmi.py

--- bmi.py
+++ bmi.py
@@ -4,7 +4,7 @@ weight = 60.0      # in kilograms
 height = 1.58      # in meters
 
 Calculate BMI using the formula: weight / (height^2)
-bmi = weight / (height * 2)
+bmi = weight / (height ** 2)
 
 Print BMI as float
 print("Susan's BMI is:", float(bmi))
