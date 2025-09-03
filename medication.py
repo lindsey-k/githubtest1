e6cca780616c20a163d4044764c679cacd59ae2e
lindsey-k <doyoung.kwon7251@gmail.com>
2025-09-02 18:09:44 -0600

    Simple Data Type Q7

---
+++ medication.py
@@ -0,0 +1,5 @@
+# Jack has been prescribed 5 medications
+Medication = 5   # store the number of medications in the variable
+# Print the type of the variable
+print("Medication:", Medication)
+print("Type of Medication:", type(Medication))
